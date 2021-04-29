3e59913a29ad1edc48228956e76bdf18f8cdd693
dewqwer <natcha.1@mail.kmutt.ac.th>
2021-04-29 14:49:07 +0700

    แก้ไฟล์ urls.py

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -18,9 +18,12 @@ from django.urls import path
 
 from app import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/', views.home),
     path('homepage/page2', views.page2, name="page2"),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
